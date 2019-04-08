c9f702b697704a818d697884ce7ce2f253a06baf
Min <Min@LAPTOP-D79CRQ9S>
2019-04-08 01:25:18 -0500

    1

---
+++ Team_Project/teamProject/Player.java
@@ -0,0 +1,42 @@
+package teamProject;
+
+import javax.swing.JPasswordField;
+
+public class Player
+{
+  private String username;
+  private int score;
+  private JPasswordField passwordField;
+
+  //Setters and Getters
+  public void setUsername(String username)
+  {
+    this.username = username;
+  }
+  
+  public String getUsername()
+  {
+    return username;
+  }
+  
+  
+  public int getScore()
+  {
+    return score;
+  }
+  
+  public void setScore(int score)
+  {
+    this.score = score;
+  }
+  
+  public JPasswordField getPasswordField()
+  {
+    return passwordField;
+  }
+  
+  public void setPasswordField(JPasswordField passwordField)
+  {
+    this.passwordField = passwordField;
+  }
+}
---
+++ Team_Project/teamProject/SpinWheelControl.java
@@ -0,0 +1,6 @@
+package teamProject;
+
+public class SpinWheelControl
+{
+
+}
