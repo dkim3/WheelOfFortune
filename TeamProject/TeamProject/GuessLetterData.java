c5bb924981d4436a3a2cca6b9a6118c8a2eb0909
dkim3 <48967524+dkim3@users.noreply.github.com>
2019-04-11 23:34:01 -0500

    4/11 Hassan

--- TeamProject/TeamProject/GuessLetterData.java
+++ TeamProject/TeamProject/GuessLetterData.java
@@ -1,30 +1,41 @@
 package TeamProject;
 
-public class GuessLetterData
-{
+import java.util.ArrayList;
+
+public class GuessLetterData {
 	private Integer prizeMoney;
 	private String wordToGuess;
-	private Character chosenLetter;
-	
+	private ArrayList<Character> chosenLetter;
+
+	public GuessLetterData(char charAt, Integer price) {
+		chosenLetter = new ArrayList<Character>();
+
+		this.chosenLetter.add(charAt);
+		this.prizeMoney = price;
+	}
+
 	public void setPrizeMoney(Integer prizeMoney) {
-		this.prizeMoney = prizeMoney;		
+		this.prizeMoney = prizeMoney;
 	}
+
 	public void setwordToGuess(String wordToGuess) {
-		this.wordToGuess = wordToGuess;		
+		this.wordToGuess = wordToGuess;
 	}
+
 	public void setchosenLetter(Character chosenLetter) {
-		this.chosenLetter = chosenLetter;		
+		this.chosenLetter.add(chosenLetter);
 	}
-	
+
 	public Integer getPrizeMoney() {
 		return prizeMoney;
 	}
+
 	public String getwordToGuess() {
 		return wordToGuess;
 	}
-	public Character getchosenLetter() {
+
+	public ArrayList<Character> getchosenLetter() {
 		return chosenLetter;
 	}
-	
-	
+
 }
