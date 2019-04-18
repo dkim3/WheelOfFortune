ebc25880adda5a4b4400f6bb78480ef773f53757
Aung Kyaw Min <48268937+AungKyaw-Min@users.noreply.github.com>
2019-04-17 23:28:02 -0500

    04/17/19 11:27 PM Min

--- TeamProject/TeamProject/SwitchPlayer.java
+++ TeamProject/TeamProject/SwitchPlayer.java
@@ -1,30 +1,51 @@
+    
 package TeamProject;
 
 public class SwitchPlayer
 {
-	private PlayerClient currentPlayer;
-	private PlayerClient nextPlayer;
+  private PlayerClient currentPlayer;
+  private PlayerClient nextPlayer;
+  private Integer score;
+  private Integer score_2;
 
-	public SwitchPlayer (PlayerClient currentPlayer, PlayerClient nextPlayer)
-	{
-		this.currentPlayer = currentPlayer;
-		this.nextPlayer = nextPlayer;
-	}
+  public Integer getScore()
+  {
+    return score;
+  }
 
-	public PlayerClient getCurrentPlayer () {
-		return currentPlayer;
-	}
-	public PlayerClient getNextPlayer () {
-		return nextPlayer;
-	}
-	public void setCurrentPlayer (PlayerClient CurrentPlayer)
-	{
-		this.currentPlayer= CurrentPlayer;
-	}
-	public void setNextPlayer (PlayerClient nextPlayer)
-	{
-		this.nextPlayer= nextPlayer;
-	}
+  public void setScore(Integer score)
+  {
+    this.score = score;
+  }
 
+  public Integer getScore_2()
+  {
+    return score_2;
+  }
 
+  public void setScore_2(Integer score_2)
+  {
+    this.score_2 = score_2;
+  }
+
+  public SwitchPlayer (PlayerClient currentPlayer, PlayerClient nextPlayer)
+  {
+    this.currentPlayer = currentPlayer;
+    this.nextPlayer = nextPlayer;
+  }
+
+  public PlayerClient getCurrentPlayer () {
+    return currentPlayer;
+  }
+  public PlayerClient getNextPlayer () {
+    return nextPlayer;
+  }
+  public void setCurrentPlayer (PlayerClient CurrentPlayer)
+  {
+    this.currentPlayer= CurrentPlayer;
+  }
+  public void setNextPlayer (PlayerClient nextPlayer)
+  {
+    this.nextPlayer= nextPlayer;
+  }
 }
