bcb2a160205c089e7167bd7e5c67c832476799a9
Dayo <Dayo@10.252.146.125>
2019-04-22 00:07:47 -0500

    4/22 DaEl

--- TeamProject/TeamProject/SwitchPlayer.java
+++ TeamProject/TeamProject/SwitchPlayer.java
@@ -11,7 +11,15 @@ public class SwitchPlayer implements Serializable {
 	private Integer score;
 	private Integer score_2;
 	private Integer letterLeft;
+	
+	
+	private PlayerClient currentPlayer;
+	private PlayerClient nextPlayer;
+	private ArrayList<Character> lettersSoFar;
 
+	
+
+	
 	public SwitchPlayer() {
 		prizeMoney =0;
 		wordToGuess="";
@@ -86,45 +94,37 @@ public class SwitchPlayer implements Serializable {
 	}
 	
 	
-//	
-//	
-//	
-//	private PlayerClient currentPlayer;
-//	private PlayerClient nextPlayer;
-//	private ArrayList<Character> lettersSoFar;
-//	
-//
-//	public SwitchPlayer() {
-//		lettersSoFar = new ArrayList<Character>();
-//	}
-//
-//	public SwitchPlayer(PlayerClient currentPlayer, PlayerClient nextPlayer) {
-//		lettersSoFar = new ArrayList<Character>();
-//		this.currentPlayer = currentPlayer;
-//		this.nextPlayer = nextPlayer;
-//	}
-//
-//	public PlayerClient getCurrentPlayer() {
-//		return currentPlayer;
-//	}
-//
-//	public PlayerClient getNextPlayer() {
-//		return nextPlayer;
-//	}
-//
-//	public void setCurrentPlayer(PlayerClient CurrentPlayer) {
-//		this.currentPlayer = CurrentPlayer;
-//	}
-//
-//	public void setNextPlayer(PlayerClient nextPlayer) {
-//		this.nextPlayer = nextPlayer;
-//	}
-//	
-//	public ArrayList<Character> getLettersSoFar() {
-//		return lettersSoFar;
-//	}
-//
-//	public void setLettersSoFar(ArrayList<Character> lettersSoFar) {
-//		this.lettersSoFar = lettersSoFar;
-//	}
+	
+	
+
+
+	public SwitchPlayer(PlayerClient currentPlayer, PlayerClient nextPlayer) {
+		lettersSoFar = new ArrayList<Character>();
+		this.currentPlayer = currentPlayer;
+		this.nextPlayer = nextPlayer;
+	}
+
+	public PlayerClient getCurrentPlayer() {
+		return currentPlayer;
+	}
+
+	public PlayerClient getNextPlayer() {
+		return nextPlayer;
+	}
+
+	public void setCurrentPlayer(PlayerClient CurrentPlayer) {
+		this.currentPlayer = CurrentPlayer;
+	}
+
+	public void setNextPlayer(PlayerClient nextPlayer) {
+		this.nextPlayer = nextPlayer;
+	}
+	
+	public ArrayList<Character> getLettersSoFar() {
+		return lettersSoFar;
+	}
+
+	public void setLettersSoFar(ArrayList<Character> lettersSoFar) {
+		this.lettersSoFar = lettersSoFar;
+	}
 }
