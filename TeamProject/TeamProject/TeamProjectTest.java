5004847260a94d45af335e318caa9cf83359de62
nvk1196 <48897412+nvk1196@users.noreply.github.com>
2019-04-22 21:12:37 -0500

    Create TeamProjectTest.java

---
+++ TeamProject/TeamProject/TeamProjectTest.java
@@ -0,0 +1,53 @@
+package TeamProject;
+import static org.junit.Assert.*;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+
+public class TeamProjectTest
+{ 
+  private Database db;  
+  private int rando;
+  private ServerGUI serverGUI;
+  private PlayerGUI clientGUI;
+  private PlayerClient client;
+  String [] animals = {"chimpanzee","octopus","porcupine","sheep","giraffe"};
+  
+  @Before
+  public void setUp() throws Exception 
+  {
+    db = new Database(); 
+    rando = ((int)Math.random()*animals.length); 
+
+  }
+  @Test
+  public void testDatabaseConnection() throws FileNotFoundException
+  {
+    Connection con = db.getConnection();
+    
+    //check if database is connected
+    assertNotNull("Check Database Connection", con);   
+  }
+  
+  @Test
+  public void testQuery() throws SQLException  
+  {
+    String expected = animals[rando];   
+    ArrayList<String> animals_in_databse = db.query("select words from animals where words='" + expected +"';");
+    String actual_animal = animals_in_databse.get(0);
+    
+    //compare expected with animals_in_databse using assertEquals
+     assertEquals("Animal comparision", expected, actual_animal);
+  }
+  
+  
+  
+}
